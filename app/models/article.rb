d6f28d58e46b69f690ba187ddda8a78ce09c50e2
David Kramer <david@thekramers.nets>
2017-11-11 17:20:24 -0500

    Added data validation so title, description, author are required

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+    validates :title, presence: true, length: { minimum: 5 }
+    validates :description, presence: true, length: { minimum: 5 }
+    validates :author, presence: true, length: { minimum: 5 }
+
 end
